671d3db5924c54a3de95b84f45aaba1973a0e9ef
leo-holanda <leo.uldholanda@gmail.com>
2019-11-08 18:44:10 -0300

    change email host and schema minor fix

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_10_25_163538) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "activation_digest"
-    t.boolean "activated"
+    t.boolean "activated", default: false
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
